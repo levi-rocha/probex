205889399533f887962b64599502b8ee47e05f49
Levi Rocha <ilevidr@gmail.com>
2017-03-24 20:44:32 -0300

    DAO implementado

--- ex/ModuloEJB/ejbModule/br/unifor/bean/AlunoBean.java
+++ ex/ModuloEJB/ejbModule/br/unifor/bean/AlunoBean.java
@@ -1,8 +1,11 @@
 package br.unifor.bean;
 
-import java.util.ArrayList;
 import java.util.Collection;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+
+import br.unifor.dao.AlunoDAO;
 import br.unifor.entity.Aluno;
 
 /**
@@ -11,81 +14,30 @@ import br.unifor.entity.Aluno;
 @Stateless
 public class AlunoBean implements AlunoBeanRemote {
 
-	private static ArrayList<Aluno> listaAlunos = new ArrayList<Aluno>();
+	@EJB
+	private AlunoDAO alunoDAO;
 
-	static {
-		Aluno a1 = new Aluno();
-		a1.setId((long) listaAlunos.size());
-		a1.setNome("Maria");
-		a1.setEmail("maria@mail.com");
-		a1.setCurso("Angular 2");
-		listaAlunos.add(a1);
-
-		Aluno a2 = new Aluno();
-		a2.setId((long) listaAlunos.size());
-		a2.setNome("Pedro");
-		a2.setEmail("pedro@mail.com");
-		a2.setCurso("Java EE");
-		listaAlunos.add(a2);
-
-	}
-
-	
 	public AlunoBean() {
-		
+
 	}
 
 	@Override
 	public Collection<Aluno> listaAlunos() {
-
-		return this.listaAlunos;
+		return this.alunoDAO.lista();
 	}
 
 	@Override
 	public String adicionaAluno(Aluno aluno) {
-
-		aluno.setId((long) this.listaAlunos.size());
-
-		if (this.listaAlunos.add(aluno))
-			return "Aluno cadastrado";
-		else
-			return "Erro ao cadastrar aluno";
+		return this.alunoDAO.insere(aluno);
 	}
 
 	@Override
 	public String removeAluno(Long id) {
-
-		if (listaAlunos.remove(buscaAlunoPorId(id)))
-			return "Aluno removido";
-		else
-			return "Erro ao remover aluno";
+		return this.alunoDAO.remove(id);
 	}
 
 	@Override
 	public String alteraAluno(Aluno aluno) {
-
-		Aluno alunoEncontrado = this.buscaAlunoPorId(aluno.getId());
-
-		if (alunoEncontrado != null) {
-			alunoEncontrado.setNome(aluno.getNome());
-			alunoEncontrado.setEmail(aluno.getEmail());
-			alunoEncontrado.setIdade(aluno.getIdade());
-			alunoEncontrado.setCurso(aluno.getCurso());
-			
-			return "Aluno atualizado";
-		} else {
-			return "Erro ao atualizar aluno";
-		}
-	}
-
-	private Aluno buscaAlunoPorId(Long id) {
-
-		Aluno alunoEncontrado = null;
-		for (Aluno aluno : this.listaAlunos) {
-			if (aluno.getId().equals(id))
-				alunoEncontrado = aluno;
-		}
-
-		return alunoEncontrado;
+		return this.alunoDAO.altera(aluno);
 	}
 }
--- ex/ModuloEJB/ejbModule/br/unifor/entity/Aluno.java
+++ ex/ModuloEJB/ejbModule/br/unifor/entity/Aluno.java
@@ -2,12 +2,17 @@ package br.unifor.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Aluno implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String nome;
 	private Integer idade;
