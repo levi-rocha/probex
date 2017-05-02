8b7ac4fc7a7b0b8733019a8a400398b676319bae
Lucas <lucasvddt2@yahoo.com.br>
2017-05-02 19:35:14 -0300

    commentBO e commentBORemote

---
+++ backend/ProbexEJB/ejbModule/br/unifor/probex/business/CommentBO.java
@@ -0,0 +1,43 @@
+package br.unifor.probex.business;
+
+import java.util.Collection;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import br.unifor.probex.dao.CommentDAO;
+import br.unifor.probex.entity.Comment;
+
+@Stateless
+public class CommentBO implements CommentBORemote {
+
+	@EJB
+	private CommentDAO commentDAO;
+
+	public CommentBO() {
+
+	}
+
+	@Override
+	public Collection<Comment> listComments() {
+		return this.commentDAO.list();
+	}
+	
+	
+	
+
+	@Override
+	public String addComment(Comment comment) {
+		return this.commentDAO.insert(comment);
+	}
+
+	@Override
+	public String removeComment(Long id) {
+		return this.commentDAO.remove(id);
+	}
+
+	@Override
+	public String updateComment(Comment comment ) {
+		return this.commentDAO.update(comment);
+	}
+}
