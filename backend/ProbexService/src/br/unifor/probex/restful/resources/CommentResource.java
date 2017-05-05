0a35a3bd8978748e8bb44e3314ec079c8cffeb4b
Levi Rocha <ilevidr@gmail.com>
2017-05-05 15:14:18 -0300

    integracao Service

--- backend/ProbexService/src/br/unifor/probex/restful/resources/CommentResource.java
+++ backend/ProbexService/src/br/unifor/probex/restful/resources/CommentResource.java
@@ -1,8 +1,14 @@
 package br.unifor.probex.restful.resources;
 
-	import javax.ejb.EJB;
+import java.util.Collection;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -10,47 +16,45 @@ import javax.ws.rs.Produces;
 import br.unifor.probex.business.CommentBORemote;
 import br.unifor.probex.entity.Comment;
 
-	@Stateless
-	@Path("/posts")
-	public class CommentResource {{
+@Stateless
+@Path("/posts")
+public class CommentResource {
 
-		@EJB
-		private CommentBORemote commentBO;
+	@EJB
+	private CommentBORemote commentBO;
 
-		@Path("{id}")
-		@GET
-		@Produces("application/json")
-		public Comment findCommentById(@PathParam("id") Long id) {
-			return commentBO.findCommentById(id);
-		}
-
-		@GET
-		@Produces("application/json")
-		public Collection<CommentDTO> listComments() {
-			return commentBO.listComments();
-		}
-
-		@POST
-		@Consumes("application/json")
-		@Produces("text/plain")
-		public String addComment(Comment comment) {
-			return commentBO.addComment(comment);
-		}
-
-		@PUT
-		@Consumes("application/json")
-		@Produces("text/plain")
-		public String updateComment() {
-			return commentBO.updateComment(comment);
-		}
-
-		@Path("{id}")
-		@DELETE
-		@Produces("text/plain")
-		public String removeComment(@PathParam("id") Long id) {
-			return commentBO.removeComment(id);
-		}
-
+	@Path("{id}")
+	@GET
+	@Produces("application/json")
+	public Comment findCommentById(@PathParam("id") Long id) {
+		return commentBO.findCommentById(id);
 	}
-	
+
+	@GET
+	@Produces("application/json")
+	public Collection<Comment> listComments() {
+		return commentBO.listComments();
+	}
+
+	@POST
+	@Consumes("application/json")
+	@Produces("text/plain")
+	public String addComment(Comment comment) {
+		return commentBO.addComment(comment);
+	}
+
+	@PUT
+	@Consumes("application/json")
+	@Produces("text/plain")
+	public String updateComment(Comment comment) {
+		return commentBO.updateComment(comment);
+	}
+
+	@Path("{id}")
+	@DELETE
+	@Produces("text/plain")
+	public String removeComment(@PathParam("id") Long id) {
+		return commentBO.removeComment(id);
+	}
+
 }
--- backend/ProbexService/src/br/unifor/probex/restful/resources/PostResource.java
+++ backend/ProbexService/src/br/unifor/probex/restful/resources/PostResource.java
@@ -1,56 +1,61 @@
 package br.unifor.probex.restful.resources;
 
-	import javax.ejb.EJB;
+import java.util.Collection;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import br.unifor.probex.business.PostBORemote;
+import br.unifor.probex.dto.PostDTO;
 import br.unifor.probex.entity.Post;
 
-	@Stateless
-	@Path("/posts")
-	public class PostResource {{
+@Stateless
+@Path("/posts")
+public class PostResource {
 
-		@EJB
-		private PostBORemote postBO;
+	@EJB
+	private PostBORemote postBO;
 
-		@Path("{id}")
-		@GET
-		@Produces("application/json")
-		public Post findPostById(@PathParam("id") Long id) {
-			return postBO.findPostById(id);
-		}
-
-		@GET
-		@Produces("application/json")
-		public Collection<PostDTO> listPosts() {
-			return postBO.listPosts();
-		}
-
-		@POST
-		@Consumes("application/json")
-		@Produces("text/plain")
-		public String addPost(Post post) {
-			return postBO.addPost(post);
-		}
-
-		@PUT
-		@Consumes("application/json")
-		@Produces("text/plain")
-		public String updatePost() {
-			return postBO.updatePost(post);
-		}
-
-		@Path("{id}")
-		@DELETE
-		@Produces("text/plain")
-		public String removePost(@PathParam("id") Long id) {
-			return postBO.removePost(id);
-		}
-
+	@Path("{id}")
+	@GET
+	@Produces("application/json")
+	public Post findPostById(@PathParam("id") Long id) {
+		return postBO.findPostById(id);
 	}
-	
+
+	@GET
+	@Produces("application/json")
+	public Collection<PostDTO> listPosts() {
+		return postBO.listPosts();
+	}
+
+	@POST
+	@Consumes("application/json")
+	@Produces("text/plain")
+	public String addPost(Post post) {
+		return postBO.addPost(post);
+	}
+
+	@PUT
+	@Consumes("application/json")
+	@Produces("text/plain")
+	public String updatePost(Post post) {
+		return postBO.updatePost(post);
+	}
+
+	@Path("{id}")
+	@DELETE
+	@Produces("text/plain")
+	public String removePost(@PathParam("id") Long id) {
+		return postBO.removePost(id);
+	}
+
 }
