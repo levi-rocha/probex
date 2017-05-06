11ce23e28d2d7fc8888a22fab05159a6c3a95c51
Levi Rocha <ilevidr@gmail.com>
2017-05-05 23:46:39 -0300

    username unique

--- backend/ProbexService/src/br/unifor/probex/restful/resources/SigninResource.java
+++ backend/ProbexService/src/br/unifor/probex/restful/resources/SigninResource.java
@@ -3,10 +3,8 @@ package br.unifor.probex.restful.resources;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -19,7 +17,7 @@ public class SigninResource {
 
 	@EJB
 	private UserBORemote userBO;
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -28,6 +26,5 @@ public class SigninResource {
 		data.setPassword(null);
 		return data;
 	}
-	
 
 }
