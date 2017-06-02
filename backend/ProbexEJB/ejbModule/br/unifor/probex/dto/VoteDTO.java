d643c9a172cc8873675ce942df9f9ecf4878ef64
Levi Rocha <ilevidr@gmail.com>
2017-06-02 20:36:53 -0300

    #33 #46 - REST para inclusao de voto

---
+++ backend/ProbexEJB/ejbModule/br/unifor/probex/dto/VoteDTO.java
@@ -0,0 +1,28 @@
+package br.unifor.probex.dto;
+
+
+public class VoteDTO {
+
+    private Long postId;
+    private String username;
+
+    public VoteDTO() {
+
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
